4451f9bac9a27daed2b1a2c1d1ba3aaac4700663
Murbong <wc100620@gmail.com>
2018-11-15 00:37:44 +0900

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -4,6 +4,8 @@ public class Computer {
 	
 	public int X,Y;
 	
+	public static int MAX = Main.MaxXY-1;
+	
 	int map_weight[][] = 
 		  { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -25,11 +27,17 @@ public class Computer {
 	public void SetWeight(int _x,int _y,int _weight) {
 		
 		if((_x%2==0||_y%2==0)) {
-			_weight = _weight*2;
+			_weight = _weight*2;//짝수이면 확률이 늘어난다.
 		}
 		
-		if(_x==0||_y==0||_x==15||_y==15) {
-			_weight = _weight*10;
+		if(_x==0||_y==0||_x==MAX||_y==MAX) {
+			_weight = _weight*10;//변 부분은 먹으면 좋다.
+		}
+		if((_x==0&&_y==0)||(_x==MAX&&_y==MAX)||(_x==0&&_y==MAX)||(_x==MAX&&_y==0)) {// 네 귀퉁이는 무조건 먹자.
+			_weight = _weight*100;//100으로 설정
+		}
+		else if((_x<=1&&_y<=1)||(_x>=MAX-1&&_y>=MAX-1)) {// 네 귀퉁이가 아닌데 2x2 사각형에 들어갈경우
+			_weight = 1;//쓰레기 가중치가 된다.
 		}
 		
 		map_weight[_x][_y] = _weight;
@@ -44,7 +52,7 @@ public class Computer {
 			
 			for(int j=0;j<16;j++) {
 				
-				System.out.print(map_weight[j][i]+" ");
+				//System.out.print(map_weight[j][i]+" ");
 				
 				if(max<map_weight[i][j]) {
 					max = map_weight[i][j];
@@ -53,10 +61,10 @@ public class Computer {
 				}
 				
 			}
-			System.out.println("");
+			//System.out.println("");
 		}
 		
-		System.out.println("max = "+max+" X = "+X+" Y : " +Y+"\n\n");
+		//System.out.println("max = "+max+" X = "+X+" Y : " +Y+"\n\n");
 	}
 
 }
